39edce8740a2c0bfb4eeaba8446c692147f78377
Rohit Varpe <rohitvarpe1340@gmai.com>
2026-07-04 18:36:50 +0530

    add reverse string

---
+++ NewPractice/string/reverse.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+string reverseString(string str ,int n ){
+    int st = 0 , end= n-1;
+    while(st < end){
+    swap(str[st], str[end]);
+    st++;
+    end--;
+    }
+    return str;
+}
+
+int main(){
+    string str = "epraV tihoR";
+    int n = str.length();
+    str = reverseString(str, n);
+    cout << str << endl;
+    return 0;
+}
