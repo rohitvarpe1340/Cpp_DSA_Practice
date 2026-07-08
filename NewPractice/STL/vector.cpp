6520cc205b7bc5cadd5ec81669599bd946d4d281
Rohit Varpe <rohitvarpe1340@gmai.com>
2026-07-08 09:56:21 +0530

    add proper vectors

--- NewPractice/STL/vector.cpp
+++ NewPractice/STL/vector.cpp
@@ -2,48 +2,72 @@
 #include <vector>
 using namespace std;
 int main(){
-     vector <int> nums;
-     nums.push_back(23);
-     nums.push_back(22);
-     nums.push_back(24);
-     nums.push_back(25);
-     nums.emplace_back(26);
 
-     nums.pop_back();
+    //vector 1
+   //  vector <int> nums;
 
-     for(int val : nums){
-        cout<<val<<" ";
+   //  nums.push_back(10);
+   //  nums.push_back(20);
+   //  nums.push_back(30);
+   //  nums.push_back(40);
+   //  nums.push_back(50);
+
+   //  cout<<nums.front()<<endl;
+   //  cout<<nums.back()<<endl;
+
+   //  nums.pop_back();
+
+   //   for(int val : nums){
+   //      cout<<val<<" ";
+   //   }  
+   //   cout<<endl;
+   //   cout<<nums.size()<<endl;
+   //   cout<<nums.capacity()<<endl;
+
+     //vector 2 
+      // vector <int> vec={1,2,3,4,5,};
+
+      // for(int val : vec){
+      //   cout<<val<<" ";
+      // }
+      // cout<<"\n";
+      //vector 3
+   //    vector <int> arr(5,10);
+
+   //    for(int val : arr){
+   //      cout<<val<<" ";
+   //    }
+   // cout<<"\n";
+      //vector 4
+      // vector <int> vec1= {1, 2, 3, 4};
+      // vector<int> vec2(vec1);
+
+      //  for(int val : vec2){
+      //   cout<<val <<" ";
+      //  }
+
+       //vector 5
+      //  vector<int> num1= {1,2,3,4,5};
+
+      //  num1.insert(num1.begin()+2, 100);
+       //num1.clear();
+       //num1.erase(num1.begin()+1, num1.begin()+3);
+
+      //   cout<<"size :"<<num1.size()<<endl;
+      //   cout<<"capacity : "<<num1.capacity()<<endl;
+
+      //vector 6 
+      vector<int> nums = {1,2,3,4,5};
+     //vector<int>::iterator vec;
+     for( auto vec= nums.begin();vec != nums.end();vec++){
+      cout<<*(vec)<<" ";
      }
-     cout<<endl;
-    
-     cout<< "front : "<<nums.front()<<endl;
-     cout<<"back : " <<nums.back()<<endl;
+     cout<<"\n";
+     for(auto vec = nums.rbegin();vec != nums.rend();vec++){
+      cout<<*(vec)<<" ";
+     }
+     
 
-    cout<<nums.at(2)<<endl;
-    cout<<endl;
-    cout<<nums.size()<<endl;
-    cout<<nums.capacity()<<endl;
-
-    vector <int> vec= { 1, 2 ,3 , 4 , 5};
-    vec.pop_back();
-    for(int val : vec){
-        cout<<val<<" ";
-    }
-    cout<<"\n";
-
-    vector <int> vec1(4 , 10);
-    for(int val : vec1){
-        cout<<val<<" ";
-    }
-    cout<<"\n";
-
-    vector <int> arr1 = {1 ,2 , 3, 4, 5};
-    vector<int> arr2(arr1);
-    for(int val : arr2){
-        cout<<val <<" ";
-    }
 
     return 0;
 }
-
-
